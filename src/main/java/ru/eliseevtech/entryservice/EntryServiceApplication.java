d4c249f16b302fe9fe256849962a07ac593cd16f
a.eliseev <aeliseev_vo@inno.tech>
2025-09-14 13:02:13 +0300

    init

---
+++ src/main/java/ru/eliseevtech/entryservice/EntryServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.eliseevtech.entryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntryServiceApplication.class, args);
+    }
+
+}
